7426b4da32f61b51ae00b57064bde64cf4eb0759
dsrawat <dharam.rawat004@gmail.com>
2022-03-11 22:32:35 +0530

    added AnnotationTransformer missing file

--- src/main/java/com/core/AnnotationTransformer.java
+++ src/main/java/com/core/AnnotationTransformer.java
@@ -1,6 +1,24 @@
 package com.core;
 
-public class WaitHandler {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.annotations.ITestAnnotation;
+
+public class AnnotationTransformer implements IAnnotationTransformer{
+
+	@Override
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		// TODO Auto-generated method stub
+		 IRetryAnalyzer retry = annotation.getRetryAnalyzer();
+		 if (retry == null)
+		 {
+			 annotation.setRetryAnalyzer(BaseClass.class);
+		 }
+	}
 	
 	
 
